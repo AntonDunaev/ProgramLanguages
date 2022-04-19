17b40af55d5df9b785c119b9a59e39ed8d25d48c
AntonDunaev <anton.dunaev>
2022-04-19 23:23:31 +0300

    написал вторую задачу

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
-﻿Console.Write("Input integer number: ");
+﻿/*Console.Write("Input integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Result is " + num*num);
+*/
+// задача 2.
+Console.Write("Input first integer number  ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number  ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int quad = num2*num2;
+if(num1 == quad)
+{
+    Console.WriteLine("First number is a quad of second number");
+}
+else
+{
+Console.WriteLine("First number is not a quad of second number");
+}
 
