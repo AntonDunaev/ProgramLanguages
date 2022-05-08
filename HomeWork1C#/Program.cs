87d97e462a97bb0a8cc9848c5a124d9732a4d28d
AntonDunaev <anton.dunaev>
2022-05-08 16:24:10 +0300

    создал проект правильно

---
+++ HomeWork1C#/Program.cs
@@ -0,0 +1,7 @@
+﻿// задача 1.
+Console.Write("Input integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int secondnum = Convert.ToInt32(Console.ReadLine());
+if(num == secondnum)
+     Console.WriteLine("Inputed nubers are equal.");
