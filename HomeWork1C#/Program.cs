9b18510772c27bdbea27f6d05cf57459bd4bdc12
AntonDunaev <anton.dunaev>
2022-05-08 16:38:10 +0300

    first task comlete

--- HomeWork1C#/Program.cs
+++ HomeWork1C#/Program.cs
@@ -5,3 +5,10 @@ Console.Write("Input second integer number: ");
 int secondnum = Convert.ToInt32(Console.ReadLine());
 if(num == secondnum)
      Console.WriteLine("Inputed nubers are equal.");
+    else
+    {
+     if(num > secondnum)
+        Console.WriteLine(" max number " + num);
+        else
+        Console.WriteLine("max number " + secondnum);
+    }
