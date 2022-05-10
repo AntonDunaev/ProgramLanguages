9afa563a2538902aa27f1109b8f5c3bfc061c7e9
AntonDunaev <anton.dunaev>
2022-05-10 22:32:08 +0300

    Metod ShowNumber

---
+++ HomeWork2C#/Program.cs
@@ -0,0 +1,16 @@
+﻿void ShowNumber()
+{
+  int num = new Random().Next(10, 99);
+  int firstNum = num / 10;
+  int secondNum = num % 10;
+  Console.Write(num + "->");
+    if (firstNum > secondNum)
+    {
+        Console.Write(firstNum);
+    }
+    else
+    {
+        Console.Write(secondNum);
+    }
+}
+ShowNumber();
