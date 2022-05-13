122c142b79cfa1433b80cc061e7ce2937847e253
AntonDunaev <anton.dunaev>
2022-05-13 19:24:37 +0300

    метод с возратом значения

--- HomeWork2C#/Program.cs
+++ HomeWork2C#/Program.cs
@@ -1,4 +1,4 @@
-﻿void ShowNumber()
+﻿/* void ShowNumber()
 {
   int num = new Random().Next(10, 99);
   int firstNum = num / 10;
@@ -14,3 +14,22 @@
     }
 }
 ShowNumber();
+*/
+// метод с возвратом значения.
+int ShowNumber() // для возврата значения необходим тип int, т.к. с типом void ничего вернуть не получится.
+{
+  int num = new Random().Next(10, 99);
+  int firstNum = num / 10;
+  int secondNum = num % 10;
+  Console.Write(num + "->");
+    if (firstNum > secondNum)
+    {
+        return firstNum;
+    }
+    else
+    {
+        return secondNum;
+    }
+}
+int a = ShowNumber();
+Console.WriteLine(a);
