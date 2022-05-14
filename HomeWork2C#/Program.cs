083b579536feb57c00d2f547501ec58426966359
AntonDunaev <anton.dunaev>
2022-05-14 21:37:31 +0300

    задача 13 пока не работает

--- HomeWork2C#/Program.cs
+++ HomeWork2C#/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Домашняя работа после второго семинара.
-void ShowSecond()
+// Задача 10. Показать вторую цифру трехзначного числа
+/*void ShowSecond()
 {
     int num = new Random().Next(100, 999);
     int firstNum = num / 10;
@@ -7,5 +8,42 @@ void ShowSecond()
     Console.Write(num + "->");
     Console.Write(secondNum);
     }
-Console.WriteLine("Задача 1. Показать второе число.");
-ShowSecond();
+Console.WriteLine("Задача 10. Показать второе число.");
+ShowSecond();*/
+// Задача 11. Вывести третью цифру и сообщить, что её нет.
+/*void ShowThird(int numbers)
+{
+    int length = numbers.Length;
+    if (length>=3)
+    {
+        Console.WriteLine(numbers[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+       }
+Console.WriteLine("Задача 13. Показать третье число.");
+Console.WriteLine("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+ShowThird(num);*/
+
+/*string ShowThird(string[] numbers)
+{
+    int length = numbers.Length;
+    if (length>=3)
+    {
+        Console.WriteLine(numbers[2]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
+Console.WriteLine("Задача 13. Показать третье число.");
+Console.WriteLine("Введите целое число: ");
+string num = Console.ReadLine();
+ShowThird(num);*/
+
+
+
