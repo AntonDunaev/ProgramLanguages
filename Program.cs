3e9e66b5726c75cdec2dfa0cdade5e2ff4ea93df
AntonDunaev <anton.dunaev>
2022-05-14 22:54:16 +0300

    первая задача третьего семинара

--- Program.cs
+++ Program.cs
@@ -198,3 +198,30 @@ Console.WriteLine("Input second integer number: ");
 int secondNum = Convert.ToInt32(Console.ReadLine());
 Sqr(firstNum, secondNum);
 */
+//семинар 3
+int FindQvart(int x, int y)
+{
+    if(x>0 && y >0)
+        return 1;
+    else if(x<0 && y>0)
+        return 4;
+    else if(x>0 && y<0)
+        return 2;
+    else if(x<0 && y<0)
+        return 3;
+    else
+        return 0;    
+}
+
+Console.Write("Введите значение по оси Х: ");
+int xDot = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение по оси Y: ");
+int yDot = Convert.ToInt32(Console.ReadLine());
+
+int qvartNum = FindQvart(xDot, yDot);
+if (qvartNum == 0)
+Console.WriteLine("точка находится на осях");
+else
+Console.WriteLine("номер четверти " + qvartNum);
+
+
