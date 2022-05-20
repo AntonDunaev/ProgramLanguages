0421080e455364bf7f636dfde5d8ccd16417a5f8
AntonDunaev <anton.dunaev>
2022-05-20 15:28:44 +0300

    Вторая задача работает

--- seminar3/Program.cs
+++ seminar3/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Семинар 3.
 //Задача 1
-
+/*
 int FindQvart(int x, int y)
 {
     if(x>0 && y >0)
@@ -23,6 +23,24 @@ if (qvartNum == 0)
 Console.WriteLine("точка находится на осях");
 else
 Console.WriteLine("номер четверти " + qvartNum);
+*/
+// Задача 2. Определить координаты точки по четверти
+void FindDot(int q)
+{
+    if(q == 1)
+    Console.WriteLine("Координаты точек в заданной области находятся в пределах :x>0 и y >0");
+        else if(q == 2)
+        Console.WriteLine("Координаты точек в заданной области находятся в пределах : x>0 && y<0");
+        else if( q == 3)
+        Console.WriteLine("Координаты точек в заданной области находятся в пределах : x<0 && y<0");
+        else if(q == 4)
+        Console.WriteLine("Координаты точек в заданной области находятся в пределах : x<0 && y>0");
+        else
+        Console.WriteLine("Вводимые данные некорректны, четверть с таким номером не существует");    
+}
+Console.Write("Введите номер четверти: ");
+int qvartNum = Convert.ToInt32(Console.ReadLine());
+FindDot(qvartNum);
 
-// Задача 2
+
 
