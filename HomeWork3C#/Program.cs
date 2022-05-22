0c5f46d8e6ad1a0e0a415450a57977f9ef6bc5ca
AntonDunaev <anton.dunaev>
2022-05-22 12:16:09 +0300

    задача 19 работает.

--- HomeWork3C#/Program.cs
+++ HomeWork3C#/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Домашняя работа после 3 семинара.
+// Задача 19.Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+void TestPolindrom(string numbers)
+{
+    int length = numbers.Length;
+    if (length > 5)
+         Console.WriteLine("В числе больше 5 знаков." );
+         else
+         if(length < 5)
+            Console.WriteLine("В числе меньше 5 знаков. ");
+        else
+        if(numbers[0] == numbers[4] && numbers[1] == numbers[3])
+        Console.WriteLine("Число является палиндромом.");
+        else
+            Console.WriteLine("Третьей цифры нет");
+}        
+Console.WriteLine("Задача 19. Проверить является ли число полнидромом.");
+Console.WriteLine("Введите пятизначное целое число: ");
+string num = Console.ReadLine();
+TestPolindrom(num);
