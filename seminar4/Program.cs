f1ae58136107e49f35b12a0906c495e0d403935d
AntonDunaev <anton.dunaev>
2022-05-22 15:26:31 +0300

    вторая задача работает

--- seminar4/Program.cs
+++ seminar4/Program.cs
@@ -1,4 +1,6 @@
 ﻿// семинар №4
+//Задача 1
+/*
 int NaturalSum(int a)
 {
     int sum = 0;
@@ -17,3 +19,43 @@ int NaturalSum(int a)
 Console.Write("Введите натуральное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Сумма чисел : " + NaturalSum(num));
+*/
+// Задача 2. Написать программу, которая принимает на вход число, и выдает сообщение сколько цифр в числе.
+int GetDigitAmount(int num)
+{
+    int count = 0;
+    while(num > 0)
+    {
+    num = num/10;
+    count ++;
+    }
+    return count;
+}
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Колчисетсво разрядов: " + GetDigitAmount(number));
+
+
+
+
+
+// Задача 3. Задача написать программу, которая вычисляет факториал числа.
+/*int Factorial(int a)
+{
+    int fact = 1;
+
+    if(a < 1) return 0;
+    else
+    {
+       for(int i = 1; i <= a; i++)
+       {
+           fact = fact * i;
+           //sum += i;  строка выполняет то же действие, что и предыдущая строка
+       } 
+    }
+    return fact;
+}
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Факториал числа " + num +" равен : " + Factorial(num));
+*/
