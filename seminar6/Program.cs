f2eccd736716e9d3359ea809379f6d0b13ba52cd
AntonDunaev <anton.dunaev>
2022-06-29 21:36:49 +0300

    написаны 3 метода для создания, заполненияи вывода массива

---
+++ seminar6/Program.cs
@@ -0,0 +1,43 @@
+﻿// Двумерные массивы и рекурсия
+/*
+string[,] table = new string[2,5];
+table[1,2] = "слово";
+for( int rows = 0; rows < 2; rows++)
+{
+    for( int columns = 0; columns < 5; columns ++)
+    {
+        Console.WriteLine($"{table[rows, columns]}");
+    }
+}
+*/
+void PrintMatrix(int[,] matr)
+{
+ for( int rows = 0; rows < matr.GetLength(0); rows++)
+{
+    for( int columns = 0; columns < matr.GetLength(1); columns ++)
+    {
+        Console.Write($"{matr[rows, columns]} ");
+    }
+    Console.WriteLine();
+}
+}
+//int [,] table = new int[3,4];
+//PrintMatrix(table);
+void FillMatrixRandome(int[,] matr)
+{
+ for( int i = 0; i < matr.GetLength(0); i++)
+{
+    for( int j = 0; j < matr.GetLength(1); j ++)
+    {
+        matr[i, j] = new Random().Next(1,10);
+    }
+    Console.WriteLine();
+}
+}
+int [,] table = new int[3,4];
+PrintMatrix(table);
+FillMatrixRandome(table);
+Console.WriteLine();
+PrintMatrix(table);
+
+
